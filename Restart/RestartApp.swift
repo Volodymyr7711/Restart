86a1b2b532b3af041386917cd6aac517f395b083
Volodymyr Mendyk <volodymyrmendyk70@gmail.com>
2023-03-28 21:22:49 +0200

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Volodymyr Mendyk on 28/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
